4c10ce477f7b995562192cdb08df276068015d34
TK3171 <98242586+TK3171@users.noreply.github.com>
2022-12-04 09:57:29 +0900

    a

--- demo3.py
+++ demo3.py
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 # Hello World for Minecraft Java Edition 1.16.5
-# mcje, MCJE: Minecraft Java Edition
+# mcje, MCJE: Minecraft Java Editio
 from mcje.minecraft import Minecraft
 import param_MCJE as param
 
@@ -18,6 +17,11 @@ mc.setBlocks(57, 4, 57,  57, 64, 57,  param.GOLD_BLOCK)
 mc.setBlocks(55, 4, 58,  55, 10, 58,  param.GOLD_BLOCK)
 mc.setBlocks(55, 4, 56,  55, 10, 56,  param.GOLD_BLOCK)
 mc.setBlocks(55, 4, 54,  55, 10, 54,  param.GOLD_BLOCK)
-=======
-aiu
->>>>>>> 3f1026719b0e246dbc715bce2d756e0940b6bb3b
+
+mc.setBlocks(52, 10, 55,  52, 10, 60,  param.GOLD_BLOCK)
+
+mc.setBlocks(70, 4, 55,  75, 10, 55,  param.GOLD_BLOCK)
+
+mc.setBlocks(55, 4, 70,  55, 4, 75,  param.GOLD_BLOCK)
+mc.setBlocks(56, 5, 70,  56, 5, 75,  param.GOLD_BLOCK)
+mc.setBlocks(57, 6, 70,  57, 6, 75,  param.GOLD_BLOCK)
